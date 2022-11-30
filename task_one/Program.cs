28c3eed90ce290ac263f5765841d44398d7bcbd6
Igor Zhidkov <stork32@mail.ru>
2022-11-30 15:07:08 +0300

    Firs Task

---
+++ task_one/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите первое число: ");
+string numstrA = Console.ReadLine() ?? "";
+int numA = Convert.ToInt32(numstrA);
+Console.Write("Введите второе число: ");
+string numstrB = Console.ReadLine() ?? "";
+int numB = Convert.ToInt32(numstrB);
+int max = numA;
+int min = numB;
+
+if (numB > numA) {max = numB; min = numA;}
+else {max = numA; min = numB;}
+
+Console.Write($"Число {max} больше, чем число {min}");
+
+
+
