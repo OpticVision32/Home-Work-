3a2d5c3f7e8ce1b7a5ce5a8073645742a06d9a6a
Igor Zhidkov <stork32@mail.ru>
2022-11-30 15:36:28 +0300

    task two

---
+++ task_two/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите первое число: ");
+string numstrA = Console.ReadLine() ?? "";
+int numA = Convert.ToInt32(numstrA);
+Console.Write("Введите второе число: ");
+string numstrB = Console.ReadLine() ?? "";
+int numB = Convert.ToInt32(numstrB);
+Console.Write("Введите третье число: ");
+string numstrC = Console.ReadLine() ?? "";
+int numC = Convert.ToInt32(numstrC);
+
+int max = numA;
+
+if (max < numB) max = numB;
+if (max < numC) max = numC;
+
+Console.Write(max);
+
