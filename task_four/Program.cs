4b1d708488ea94074dc6413b401cc17512dff971
Igor Zhidkov <stork32@mail.ru>
2022-11-30 15:51:57 +0300

    task four

---
+++ task_four/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите первое число: ");
+string numstrA = Console.ReadLine() ?? "";
+int numA = Convert.ToInt32(numstrA);
+
+for (int i = 2; i <= numA; i=i+2)
+{
+    Console.Write(i+ " ");
+}
