76fa799752d0e73bdb158a31ac1f4977f678db80
Igor Zhidkov <stork32@mail.ru>
2022-11-30 15:43:57 +0300

    task three

---
+++ task_three/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите первое число: ");
+string numstrA = Console.ReadLine() ?? "";
+int numA = Convert.ToInt32(numstrA);
+
+if (numA % 2 == 0) Console.Write("Число четное");
+else Console.Write("Число нечетное");
+
