ea6a34cf281346e5313ef2f913eefcefec402af8
Igor Zhidkov <stork32@mail.ru>
2022-12-05 14:04:25 +0300

    task_thirteen

---
+++ task_thirteen/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число: ");
+int input = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+string inputStr = input.ToString();
+if (input > 99)
+{
+    Console.WriteLine($"Третья цифра - {inputStr[2]}" );
+}
+else 
+{
+    Console.WriteLine($"Третьей цифры нет");
+}
