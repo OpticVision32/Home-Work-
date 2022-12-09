c7adc280c99c7c3d0d30a655b42c6d5a439bc890
Igor Zhidkov <stork32@mail.ru>
2022-12-09 12:28:17 +0300

    task twenty five

---
+++ task_twenty-five/Program.cs
@@ -0,0 +1,35 @@
+﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16
+*/
+int GetNumber(string message)
+{
+    bool isCorrect = false;
+    int result = 0;
+    while (!isCorrect)
+
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("Введено некорректное число");
+        }
+    }
+    return result;
+}
+
+int numberA = GetNumber("Введите число A: ");
+int numberB = GetNumber("Введите число B: ");
+int result = numberA;
+for (int i = 1; i < numberB; i++)
+{
+    result = result * numberA;
+}
+
+Console.WriteLine(result);
