7e52574a1d3229083601a68fc1f6284331c6344d
Igor Zhidkov <stork32@mail.ru>
2022-12-09 13:32:31 +0300

    task twenty seven

---
+++ task_twenty-seven/Program.cs
@@ -0,0 +1,42 @@
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12
+*/
+
+int GetNumber(string msg)
+{
+    bool isCorrect = false;
+    int result = 0;
+    while (!isCorrect)
+    {
+        Console.WriteLine(msg);
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("Введено некорректное число");
+        }
+    }
+    return result;
+}
+
+int inputNumber = GetNumber("Введите число: ");
+int result = 0;
+
+
+while (inputNumber > 10)
+{
+    result = result + inputNumber % 10;
+    inputNumber = inputNumber/10;
+}
+result = result + inputNumber;
+
+Console.WriteLine(result);
+
